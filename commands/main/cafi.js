427d99f0f2a18675dcb33cebb13beb077263f05c
Ell Contrerax <sarmientodieg@gmail.com>
2025-12-13 18:01:19 -0500

    🐸◌*̥₊ AʟʏᴀBᴏᴛ - MD◌❐🌱༉

--- commands/main/cafi.js
+++ commands/main/cafi.js
@@ -7,22 +7,22 @@ export default {
 
 .꒷🌳.𖦹˙ *Sitio Web:*  
 https://cafirexos.com
-
+ .  . ︵︵•✿•︵︵ .  ◌Ⳋ𝅄
 .꒷🎍.𖦹˙ *Área de Clientes:*  
 https://cafirexos.com/clientarea.php
-
+ .  . ︵︵•✿•︵︵ .  ◌Ⳋ𝅄
 .꒷🦦.𖦹˙ *Panel:*  
 https://panel.cafirexos.com
-
+ .  . ︵︵•✿•︵︵ .  ◌Ⳋ𝅄
 .꒷🌱.𖦹˙ *Estado de Servicios:*  
 https://estado.cafirexos.com
-
+ .  . ︵︵•✿•︵︵ .  ◌Ⳋ𝅄
 .꒷🌾.𖦹˙ *Canal de WhatsApp:*  
 https://links.cafirexos.com/whatsapp/canal
-
+ .  . ︵︵•✿•︵︵ .  ◌Ⳋ𝅄
 .꒷🦩.𖦹˙ *Soporte:*  
 https://cafirexos.com/contactenos
-`;
+ .  . ︵︵•✿•︵︵ .  ◌Ⳋ𝅄`;
 
     await client.sendMessage(m.chat, {
       image: { url: "https://cdn.cafirexos.com/logos/logo_cfros_2000x2000.png" },
