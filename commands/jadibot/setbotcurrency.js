8ccf90468c6b839b71bd16c55b1499574dc48229
Ell Contrerax <sarmientodieg@gmail.com>
2025-12-14 09:23:10 -0500

    🐸◌*̥₊ AʟʏᴀBᴏᴛ - MD◌❐🌱༉

--- commands/jadibot/setbotcurrency.js
+++ commands/jadibot/setbotcurrency.js
@@ -1,13 +1,13 @@
 export default {
   command: ['setbotcurrency'],
   category: 'socket',
-  run: async (client, m, args) => {
+  run: async (client, m, args, command, text, prefix) => {
     const idBot = client.user.id.split(':')[0] + '@s.whatsapp.net'
     const config = global.db.data.settings[idBot]
     const isOwner2 = [idBot, ...global.owner.map((number) => number + '@s.whatsapp.net')].includes(m.sender)
     if (!isOwner2 && m.sender !== owner) return m.reply(mess.socket)
     const value = args.join(' ').trim()
-    if (!value) return m.reply(`✐ Debes escribir un nombre de moneda valido.\n> Ejemplo: *${prefa}setbotcurrency Coins*`)
+    if (!value) return m.reply(`✐ Debes escribir un nombre de moneda valido.\n> Ejemplo: *${prefix + command} Coins*`)
     config.currency = value
     return m.reply(`✿ Se ha cambiado la moneda del bot a *${value}*`)
   },
