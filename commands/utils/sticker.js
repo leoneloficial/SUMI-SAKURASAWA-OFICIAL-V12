92f9501e77fa88b04ceddaa31bcebb7d000bf312
Ell Contrerax <sarmientodieg@gmail.com>
2025-12-15 13:45:35 -0500

    🐸◌*̥₊ AʟʏᴀBᴏᴛ - MD◌❐🌱༉

--- commands/utils/sticker.js
+++ commands/utils/sticker.js
@@ -1,4 +1,4 @@
-import { spawn } from 'child_process'
+import fs from 'fs';
 
 export default {
 command: ['sticker', 's'],
@@ -6,62 +6,36 @@ category: 'utils',
 run: async (client, m) => {
 
 try {
+let media
 const quoted = m.quoted ? m.quoted : m
 const mime = (quoted.msg || quoted).mimetype || ''
+const d = new Date(new Date + 3600000)
+const locale = 'es-ES'
+const dias = d.toLocaleDateString(locale, {weekday: 'long'})
+const fecha = d.toLocaleDateString(locale, {day: '2-digit', month: '2-digit', year: 'numeric'})
 
-let user = globalThis.db.data.users[m.sender]
-const name = user.name
-let text1 = user.metadatos || `S'ᴛᴇʟʟᴀʀ 🧠 WᴀBᴏᴛ`
-let text2 = user.metadatos2 || `@${name}`
-
-if (mime === 'image/webp') {
-const input = await quoted.download()
-
-const ff = spawn('ffmpeg', [
-'-i', 'pipe:0',
-'-movflags', 'faststart',
-'-pix_fmt', 'yuv420p',
-'-f', 'mp4',
-'pipe:1'
-])
-
-let buffer = Buffer.alloc(0)
-
-ff.stdout.on('data', chunk => {
-buffer = Buffer.concat([buffer, chunk])
-})
-
-ff.stdin.write(input)
-ff.stdin.end()
-
-ff.on('close', async () => {
-await client.sendVideoAsSticker(
-m.chat,
-buffer,
-m,
-{ packname: text1, author: text2 }
-)
-})
-
-return
-}
+let user = globalThis.db.data.users[m.sender];
+const name = user.name;
+let text1 = user.metadatos || `S'ᴛᴇʟʟᴀʀ 🧠 WᴀBᴏᴛ`;
+let text2 = user.metadatos2 || `@${name}`;
 
 if (/image/.test(mime)) {
-const media = await quoted.download()
-let enc = await client.sendImageAsSticker(m.chat, media, m, { packname: text1, author: text2 })
-return
+media = await quoted.download()  
+let encmedia = await client.sendImageAsSticker(m.chat, media, m, { packname: text1, author: text2})
+await fs.unlinkSync(encmedia)  
+} else if (/video/.test(mime)) {
+if ((quoted.msg || quoted).seconds > 20) {
+return m.reply('ꕥ El video no puede ser muy largo')
 }
+media = await quoted.download()
 
-if (/video/.test(mime)) {
-if ((quoted.msg || quoted).seconds > 20) return m.reply('ꕥ El video no puede ser muy largo')
-const media = await quoted.download()
-let enc = await client.sendVideoAsSticker(m.chat, media, m, { packname: text1, author: text2 })
-return
+let encmedia = await client.sendVideoAsSticker(m.chat, media, m, { packname: text1, author: text2})
+await new Promise((resolve) => setTimeout(resolve, 2000))
+await fs.unlinkSync(encmedia)  
+} else {
+return client.reply(m.chat, '✐ Por favor, envia una imagen o video para hacer un sticker.', m);
 }
-
-return client.reply(m.chat, '✐ Por favor, envia una imagen, video o sticker animado.', m)
-
 } catch (e) {
-m.reply('Error: ' + e)
+await client.sendVideoAsSticker(m.chat, media, m, { packname: text1, author: text2})
 }}
-}
+};
